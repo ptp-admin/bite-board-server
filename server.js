300bee7ee1ed3040e78b81d12bce0696afdf4c04
Beau Jorgensen <beau@coviu.com>
2023-02-19 22:27:58 +1000

    Working Server

---
+++ server.js
@@ -0,0 +1,74 @@
+
+const express = require('express')
+const db = require('./utils/database')
+const app = express()
+
+const port = 3000
+
+app.use(express.json())
+
+app.listen(port, () => {
+  console.log(`Server started. Listening on port ${port}`)
+})
+
+app.get('/ingredients', (req, res) => {
+  db
+    .select()
+    .from('ingredients')
+    .then(response => res.send(response))
+})
+
+app.get('/ingredients/:id', (req, res) => {
+  db
+    .select()
+    .from('ingredients')
+    .where('id', req.params.id)
+    .then(response => res.send(response))
+})
+
+app.post('/ingredients', (req, res) => {
+  db('ingredients')
+    .insert({
+      name: req.body.name, 
+      category: req.body.category,
+      cost_per: req.body.cost_per,
+      number_of: req.body.number_of,
+      measurement_unit: req.body.measurement_unit
+    })
+    .then(() => {
+      db
+        .select().from('ingredients')
+        .then(response => res.send(response))
+    })
+})
+
+app.put('/ingredients/:id', (req, res) => {
+  db('ingredients')
+  .where('id', req.params.id)
+  .update({
+    name: req.body.name, 
+    category: req.body.category,
+    cost_per: req.body.cost_per,
+    number_of: req.body.number_of,
+    measurement_unit: req.body.measurement_unit
+  })
+  .then(() => {
+    db
+      .select().from('ingredients')
+      .then(response => res.send(response))
+  })
+})
+
+app.delete('/ingredients/:id', (req, res) => {
+  const id = req.params.id
+  
+  db('ingredients')
+    .where('id', id)
+    .del()
+    .then(console.log(`deleted record with id: ${id}`))
+    .then(() => {
+      db
+        .select().from('ingredients')
+        .then(response => res.send(response))
+    })
+})
