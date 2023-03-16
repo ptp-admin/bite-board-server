8668252393c0a408510575cb65a3d49076575584
Beau Jorgensen <beau@coviu.com>
2023-03-16 21:00:54 +1000

    make routes better

--- server.js
+++ server.js
@@ -3,15 +3,16 @@ const express = require('express')
 const db = require('./utils/database')
 const app = express()
 
-const port = 3000
+const port = 3456
 
 app.use(express.json())
 
 app.listen(port, () => {
   console.log(`Server started. Listening on port ${port}`)
 })
-test
+
 app.get('/ingredients', (req, res) => {
+  console.log('GET request recived');
   db
     .select()
     .from('ingredients')
@@ -27,6 +28,8 @@ app.get('/ingredients/:id', (req, res) => {
 })
 
 app.post('/ingredients', (req, res) => {
+  console.log('POST request recieved');
+  console.log(req.body);
   db('ingredients')
     .insert({
       name: req.body.name, 
@@ -35,11 +38,7 @@ app.post('/ingredients', (req, res) => {
       number_of: req.body.number_of,
       measurement_unit: req.body.measurement_unit
     })
-    .then(() => {
-      db
-        .select().from('ingredients')
-        .then(response => res.send(response))
-    })
+    .then(() => res.send('Added ingredient to database'))
 })
 
 app.put('/ingredients/:id', (req, res) => {
@@ -52,11 +51,7 @@ app.put('/ingredients/:id', (req, res) => {
     number_of: req.body.number_of,
     measurement_unit: req.body.measurement_unit
   })
-  .then(() => {
-    db
-      .select().from('ingredients')
-      .then(response => res.send(response))
-  })
+  .then(() => res.send('Updated ingredient in database'))
 })
 
 app.delete('/ingredients/:id', (req, res) => {
