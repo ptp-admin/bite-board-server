a53041087d24b246e1668a48ca958c98f3b68498
Beau Jorgensen <beau@coviu.com>
2023-03-28 22:11:09 +1000

    getRecipeIngredients function added

--- controllers/recipes.js
+++ controllers/recipes.js
@@ -3,47 +3,48 @@ const db = require('../utils/database')
 
 recipesRouter.get('/', (req, res) => {
   console.log('GET request recieved');
-	// TODO: implement this whole route as a transaction for better performance. Knex has docs for transactions
-  db
+	
+	db
     .select()
     .from('recipe')
     .then(recipes => {
-			// TODO separate this next part out into a function, call it like this: getRecipeIngredients(recipes)
-			var recipesWithIngredients = recipes.map(recipe => { 
-				return db
-					.select(
-						'ingredient.*',
-					)
-					.select(db.ref('recipe_ingredient.number_of').as('recipe_number_of'))
-					.select(db.ref('recipe_ingredient.measurement_unit').as('recipe_measurement_unit'))
-					.from('recipe_ingredient')
-					.where('recipe_id', recipe.id)
-					.join('ingredient', 'ingredient.id', '=', 'recipe_ingredient.ingredient_id')
-					.then(ingredientsArray => {
-						return {
-							...recipe,
-							// TODO: separate this out into a function, call it like this: getRecipeIngredientDetails(ingredients)
-							ingredients: ingredientsArray.map(ingredient =>{ 
-								// destructure the properties returned by the above SQL query
-								const { id, name, category, cost_per, number_of, recipe_number_of, recipe_measurement_unit } = ingredient
-								
-								// reassemble the desired ingredient object
-								return {
-									id,
-									name,
-									category,
-									number_of: recipe_number_of,
-									measurement_unit: recipe_measurement_unit,
-									derived_cost: (cost_per / number_of) * recipe_number_of, // TODO: write a function which handles this, calculateDerivedCost(ingredient). Account for cases where ingredient.measurement_unit and recipe_ingredient.measurement_unit need to be converted to calculate cost.
-									ingredient_cost_per: cost_per
-								}
-							})}
-					})
-			})
-			Promise.all(recipesWithIngredients).then(results => {
+			Promise.all(getRecipeIngredients(recipes)).then(results => {
 				res.send(results)
 			})
 		})
 })
+
+const getRecipeIngredients = recipes => {
+	return recipes.map(recipe => { 
+		return db
+			.select(
+				'ingredient.*',
+			)
+			.select(db.ref('recipe_ingredient.number_of').as('recipe_number_of'))
+			.select(db.ref('recipe_ingredient.measurement_unit').as('recipe_measurement_unit'))
+			.from('recipe_ingredient')
+			.where('recipe_id', recipe.id)
+			.join('ingredient', 'ingredient.id', '=', 'recipe_ingredient.ingredient_id')
+			.then(ingredientsArray => {
+				return {
+					...recipe,
+					ingredients: ingredientsArray.map(ingredient =>{ 
+						// destructure the properties returned by the above SQL query
+						const { id, name, category, cost_per, number_of, recipe_number_of, recipe_measurement_unit } = ingredient
+						
+						// reassemble the desired ingredient object
+						return {
+							id,
+							name,
+							category,
+							number_of: recipe_number_of,
+							measurement_unit: recipe_measurement_unit,
+							derived_cost: (cost_per / number_of) * recipe_number_of, // TODO: write a function which handles this, calculateDerivedCost(ingredient). Account for cases where ingredient.measurement_unit and recipe_ingredient.measurement_unit need to be converted to calculate cost.
+							ingredient_cost_per: cost_per
+						}
+					})}
+			})
+	})
+}
 
 module.exports = recipesRouter
