2439f9feed1414d549fb00d72916d966aec69a50
paddy <paddy.r.osullivan@gmail.com>
2023-03-28 21:06:22 +1000

    move all ingredients endpoints to ingredients.js

--- server.js
+++ server.js
@@ -15,64 +15,8 @@ app.listen(port, () => {
 })
 
 // TODO: separate '/ingredients' routes into another file
-// app.get('/ingredients', (req, res) => {
-//   console.log('GET request recieved');
-//   db
-//     .select()
-//     .from('ingredient')
-//     .then(response => res.send(response))
-// })
-
 app.use('/ingredients', ingredientsRouter);
 
-// app.get('/ingredients/:id', (req, res) => {
-//   db
-//     .select()
-//     .from('ingredient')
-//     .where('id', req.params.id)
-//     .then(response => res.send(response))
-// })
-
-// app.post('/ingredients', (req, res) => {
-//   console.log('POST request recieved');
-//   console.log(req.body);
-//   db('ingredient')
-//     .insert({
-//       name: req.body.name, 
-//       category: req.body.category,
-//       cost_per: req.body.cost_per,
-//       number_of: req.body.number_of,
-//       measurement_unit: req.body.measurement_unit
-//     })
-//     .then(() => res.send('Added ingredient to database'))
-// })
-
-// app.put('/ingredients/:id', (req, res) => {
-//   db('ingredient')
-//   .where('id', req.params.id)
-//   .update({
-//     name: req.body.name, 
-//     category: req.body.category,
-//     cost_per: req.body.cost_per,
-//     number_of: req.body.number_of,
-//     measurement_unit: req.body.measurement_unit
-//   })
-//   .then(() => res.send('Updated ingredient in database'))
-// })
-
-// app.delete('/ingredients/:id', (req, res) => {
-//   const id = req.params.id
-  
-//   db('ingredient')
-//     .where('id', id)
-//     .del()
-//     .then(console.log(`deleted record with id: ${id}`))
-//     .then(() => {
-//       db
-//         .select().from('ingredient')
-//         .then(response => res.send(response))
-//     })
-// })
 
 // // TODO: separate '/recipes' routes into another file
 // app.get('/recipes', (req, res) => {
