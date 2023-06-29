26c3b3cef446f9c655e44c7a4db187b10d9ecc2c
paddy <paddy.r.osullivan@gmail.com>
2023-06-29 21:04:28 +1000

    basic shoppingListRouter setup

--- server.ts
+++ server.ts
@@ -1,21 +1,23 @@
-require('dotenv').config()
+require('dotenv').config();
 
-const express = require('express')
-const db = require('./utils/database')
-const app = express()
-const ingredientsRouter = require('./controllers/ingredients')
-const recipesRouter = require('./controllers/recipes')
+const express = require('express');
+const db = require('./utils/database');
+const app = express();
+const ingredientsRouter = require('./controllers/ingredients');
+const recipesRouter = require('./controllers/recipes');
+const shoppingListRouter = require('./controllers/shoppingList');
 
-const port = process.env.PORT
+const port = process.env.PORT;
 
-app.use(express.json())
-app.use(express.static('build'))
+app.use(express.json());
+app.use(express.static('build'));
 
 app.use('/ingredients', ingredientsRouter);
 app.use('/recipes', recipesRouter);
+app.use('/shoppingList', shoppingListRouter);
 
 app.listen(port, () => {
-  console.log(`Server started. Listening on port ${port}`)
-})
+  console.log(`Server started. Listening on port ${port}`);
+});
 
 export {};
