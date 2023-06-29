7fec169d5a8404cb2d3c1eeea656d4e430a5518c
paddy <paddy.r.osullivan@gmail.com>
2023-06-29 21:08:49 +1000

    sketch out crud operations

--- controllers/shoppingList.ts
+++ controllers/shoppingList.ts
@@ -13,8 +13,25 @@ interface ShoppingList {
   createdAt: string; // TODO maybe convert into a timestamp?
 }
 
+shoppingListRouter.post('/', async (req: any, res: any) => {
+  // create a shopping list here
+});
+
+shoppingListRouter.put('/:id', async (req: any, res: any) => {
+  // updating a shopping list here
+});
+
 shoppingListRouter.get('/', async (req: any, res: any) => {
   res.send('You got a shopping list!');
+  // reading all shopping lists
+});
+
+shoppingListRouter.get('/:id', async (req: any, res: any) => {
+  // reading a shopping list
+});
+
+shoppingListRouter.delete('/:id', async (req: any, res: any) => {
+  // deleting a shopping list
 });
 
 module.exports = shoppingListRouter;
