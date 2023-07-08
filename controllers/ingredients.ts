bc6cc38aec00833da4e7944af2fb5ac47faa3199
beau-haldane <62363339+beau-haldane@users.noreply.github.com>
2023-07-08 13:50:31 +1000

    Implement DbRecipe and Recipe types across recipe routes (#3)
    * improve recipe types
    
    * add .env to gitignore lol

--- controllers/ingredients.ts
+++ controllers/ingredients.ts
@@ -8,29 +8,27 @@ const convert = require('convert-units');
 ingredientsRouter.get('/', (req: any, res: any) => {
   console.log('/ingredients/ GET request recieved');
 
-	try {
-		db.select()
-    .from('ingredient')
-    .then((response: DbIngredient[]) => {
-      const ingredients = response.map(
-        (ingredient): Ingredient => {
+  try {
+    db.select()
+      .from('ingredient')
+      .then((response: DbIngredient[]) => {
+        const ingredients = response.map((ingredient): Ingredient => {
           return {
             id: ingredient.id,
             name: ingredient.name,
             category: ingredient.category,
             costPer: ingredient.cost_per,
             measurementUnit: ingredient.measurement_unit,
-            numberOf: ingredient.number_of
+            numberOf: ingredient.number_of,
           };
-        }
-      );
+        });
 
-      res.send(ingredients);
-    });
-	} catch (error) {
-		console.error(error);
-		res.status(500).send(error);
-	}
+        res.send(ingredients);
+      });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
 });
 
 ingredientsRouter.get('/measurementUnits', cors(), (req: any, res: any) => {
@@ -69,83 +67,84 @@ ingredientsRouter.get('/measurementUnits', cors(), (req: any, res: any) => {
 });
 
 ingredientsRouter.get('/:id', (req: any, res: any) => {
-	try {
-		db.select()
-    .from('ingredient')
-    .where('id', req.params.id)
-    .then((ingredient: DbIngredient) => {
-			const response: Ingredient = {
-				id: ingredient.id,
-				name: ingredient.name,
-				category: ingredient.category,
-				costPer: ingredient.cost_per,
-				measurementUnit: ingredient.measurement_unit,
-				numberOf: ingredient.number_of
-			}
-			res.send(response)
-		});
-	} catch (error) {
-		console.error(error);
-		res.status(500).send(error);
-	}
+  try {
+    db.select()
+      .from('ingredient')
+      .where('id', req.params.id)
+      .then((ingredient: DbIngredient) => {
+        const response: Ingredient = {
+          id: ingredient.id,
+          name: ingredient.name,
+          category: ingredient.category,
+          costPer: ingredient.cost_per,
+          measurementUnit: ingredient.measurement_unit,
+          numberOf: ingredient.number_of,
+        };
+        res.send(response);
+      });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
 });
 
 ingredientsRouter.post('/', (req: any, res: any) => {
   console.log('/ingredients/ POST request recieved');
-	const ingredient: DbIngredient = {
-		name: req.body.name,
-		category: req.body.category,
-		cost_per: req.body.costPer,
-		number_of: req.body.numberOf,
-		measurement_unit: req.body.measurementUnit || '',
-	}
-  
-	try {
-		db('ingredient')
-    .insert(ingredient)
-    .then(() => res.send('Added ingredient to database'));
-	} catch (error) {
-		console.error(error);
-		res.status(500).send(error);
-	}
+  const ingredient: DbIngredient = {
+    name: req.body.name,
+    category: req.body.category,
+    cost_per: req.body.costPer,
+    number_of: req.body.numberOf,
+    measurement_unit: req.body.measurementUnit || '',
+  };
+
+  try {
+    db('ingredient')
+      .insert(ingredient)
+      .then(() => res.send('Added ingredient to database'));
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
 });
 
 ingredientsRouter.put('/:id', (req: any, res: any) => {
   console.log('/ingredients/ PUT request recieved');
-	const ingredient: DbIngredient = {
-		name: req.body.name,
-		category: req.body.category,
-		cost_per: req.body.costPer,
-		number_of: req.body.numberOf,
-		measurement_unit: req.body.measurementUnit || '',
-	}
+  const ingredient: DbIngredient = {
+    name: req.body.name,
+    category: req.body.category,
+    cost_per: req.body.costPer,
+    number_of: req.body.numberOf,
+    measurement_unit: req.body.measurementUnit || '',
+  };
 
-	try {
-		db('ingredient')
-    .where('id', req.params.id)
-    .update(ingredient)
-    .then(() => res.send('Updated ingredient in database'));
-	} catch (error) {
-		console.error(error);
-		res.status(500).send(error);
-	}
+  try {
+    db('ingredient')
+      .where('id', req.params.id)
+      .update(ingredient)
+      .then(() => res.send('Updated ingredient in database'));
+  } catch (error) {
+    console.error(error);
+    res.status(500).send(error);
+  }
 });
 
 ingredientsRouter.delete('/:id', (req: any, res: any) => {
   console.log('/ingredients/ DELETE request recieved');
   const id = req.params.id;
 
-	try {
-		db('ingredient')
-    .where('id', id)
-    .del()
-    .then(console.log(`deleted record with id: ${id}`))
-    .then(() => {
-      db.select()
-        .from('ingredient')
-        .then((response: any) => res.send(response));
-    });
-	} catch (error) { // TODO: This aint working
+  try {
+    db('ingredient')
+      .where('id', id)
+      .del()
+      .then(console.log(`deleted record with id: ${id}`))
+      .then(() => {
+        db.select()
+          .from('ingredient')
+          .then((response: any) => res.send(response));
+      });
+  } catch (error) {
+    // TODO: This aint working
     console.error(error);
     res.status(500).send(error);
   }
