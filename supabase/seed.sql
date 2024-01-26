403946bc1a05d0c6ad7f793d701638c966148d65
beau-haldane <beaujorgensen@gmail.com>
2024-01-26 17:24:07 +1000

    update seed file with more realistic data

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,29 +1,32 @@
 -- Seed the ingredient table
 INSERT INTO public.ingredient (id, name, category, cost_per, number_of, measurement_unit)
 VALUES 
-  (uuid_generate_v4(), 'Ingredient 1', 'Category 1', 1.00, 1, 'Unit 1'),
-  (uuid_generate_v4(), 'Ingredient 2', 'Category 2', 2.00, 2, 'Unit 2'),
-  (uuid_generate_v4(), 'Ingredient 3', 'Category 3', 3.00, 3, 'Unit 3'),
-  (uuid_generate_v4(), 'Ingredient 4', 'Category 4', 4.00, 4, 'Unit 4'),
-  (uuid_generate_v4(), 'Ingredient 5', 'Category 5', 5.00, 5, 'Unit 5');
+  (uuid_generate_v4(), 'Potatoes', 'Vegetables', 3.50, 1, 'kg'),
+  (uuid_generate_v4(), 'Apricots', 'Fruit', 4.13, 1, 'lb'),
+  (uuid_generate_v4(), 'Organic Wholemeal Flour', 'Baking', 5.69, 500, 'g'),
+  (uuid_generate_v4(), 'Vanilla Bean Paste', 'Baking', 14.00, 1, 'tsp'),
+  (uuid_generate_v4(), 'Kangaroo Mince', 'Meat', 12.50, 1, 'kg');
 
 -- Seed the recipe table
 INSERT INTO public.recipe (id, name, method, servings)
 VALUES 
-  (uuid_generate_v4(), 'Recipe 1', 'Method 1', 1),
-  (uuid_generate_v4(), 'Recipe 2', 'Method 2', 2),
-  (uuid_generate_v4(), 'Recipe 3', 'Method 3', 3),
-  (uuid_generate_v4(), 'Recipe 4', 'Method 4', 4),
-  (uuid_generate_v4(), 'Recipe 5', 'Method 5', 5);
+  (uuid_generate_v4(), 'Recipe 1', 'Method 1', 10),
+  (uuid_generate_v4(), 'Recipe 2', 'Method 2', 8),
+  (uuid_generate_v4(), 'Recipe 3', 'Method 3', 6),
+  (uuid_generate_v4(), 'Recipe 4', 'Method 4', 10),
+  (uuid_generate_v4(), 'Recipe 5', 'Method 5', 6);
 
 -- Seed the recipe_ingredient table
 INSERT INTO public.recipe_ingredient (recipe_id, ingredient_id, number_of, measurement_unit)
 VALUES 
-  ((SELECT id FROM public.recipe LIMIT 1), (SELECT id FROM public.ingredient LIMIT 1), 1, 'Unit 1'),
-  ((SELECT id FROM public.recipe LIMIT 1 OFFSET 1), (SELECT id FROM public.ingredient LIMIT 1 OFFSET 1), 2, 'Unit 2'),
-  ((SELECT id FROM public.recipe LIMIT 1 OFFSET 2), (SELECT id FROM public.ingredient LIMIT 1 OFFSET 2), 3, 'Unit 3'),
-  ((SELECT id FROM public.recipe LIMIT 1 OFFSET 3), (SELECT id FROM public.ingredient LIMIT 1 OFFSET 3), 4, 'Unit 4'),
-  ((SELECT id FROM public.recipe LIMIT 1 OFFSET 4), (SELECT id FROM public.ingredient LIMIT 1 OFFSET 4), 5, 'Unit 5');
+  ((SELECT id FROM public.recipe LIMIT 1), (SELECT id FROM public.ingredient LIMIT 1), 500, 'g'),
+  ((SELECT id FROM public.recipe LIMIT 1), (SELECT id FROM public.ingredient LIMIT 1 OFFSET 1), 1, 'kg'),
+  ((SELECT id FROM public.recipe LIMIT 1 OFFSET 1), (SELECT id FROM public.ingredient LIMIT 1 OFFSET 1), 2, 'kg'),
+  ((SELECT id FROM public.recipe LIMIT 1 OFFSET 3), (SELECT id FROM public.ingredient LIMIT 1 OFFSET 2), 4, 'cup'),
+  ((SELECT id FROM public.recipe LIMIT 1 OFFSET 2), (SELECT id FROM public.ingredient LIMIT 1 OFFSET 2), 1, 'kg'),
+  ((SELECT id FROM public.recipe LIMIT 1 OFFSET 3), (SELECT id FROM public.ingredient LIMIT 1 OFFSET 3), 4, 'cup'),
+  ((SELECT id FROM public.recipe LIMIT 1 OFFSET 4), (SELECT id FROM public.ingredient LIMIT 1 OFFSET 4), 500, 'g'),
+  ((SELECT id FROM public.recipe LIMIT 1 OFFSET 2), (SELECT id FROM public.ingredient LIMIT 1 OFFSET 4), 500, 'g');
 
 -- Seed the shopping_list table
 INSERT INTO public.shopping_list (id, name)
@@ -39,6 +42,10 @@ INSERT INTO public.shopping_list_recipe (shopping_list_id, recipe_id, servings)
 VALUES 
   ((SELECT id FROM public.shopping_list LIMIT 1), (SELECT id FROM public.recipe LIMIT 1), 1),
   ((SELECT id FROM public.shopping_list LIMIT 1 OFFSET 1), (SELECT id FROM public.recipe LIMIT 1 OFFSET 1), 2),
+  ((SELECT id FROM public.shopping_list LIMIT 1 OFFSET 1), (SELECT id FROM public.recipe LIMIT 1), 2),
+  ((SELECT id FROM public.shopping_list LIMIT 1 OFFSET 2), (SELECT id FROM public.recipe LIMIT 1 OFFSET 3), 3),
   ((SELECT id FROM public.shopping_list LIMIT 1 OFFSET 2), (SELECT id FROM public.recipe LIMIT 1 OFFSET 2), 3),
+  ((SELECT id FROM public.shopping_list LIMIT 1 OFFSET 3), (SELECT id FROM public.recipe LIMIT 1 OFFSET 2), 4),
   ((SELECT id FROM public.shopping_list LIMIT 1 OFFSET 3), (SELECT id FROM public.recipe LIMIT 1 OFFSET 3), 4),
+  ((SELECT id FROM public.shopping_list LIMIT 1 OFFSET 3), (SELECT id FROM public.recipe LIMIT 1 OFFSET 4), 4),
   ((SELECT id FROM public.shopping_list LIMIT 1 OFFSET 4), (SELECT id FROM public.recipe LIMIT 1 OFFSET 4), 5);
