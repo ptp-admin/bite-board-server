be46164b76cecfd8a2b0ea1753bf7ff9dcda692c
beau-haldane <beaujorgensen@gmail.com>
2024-01-26 14:07:23 +1000

    expose types for new views

--- types/types.ts
+++ types/types.ts
@@ -5,6 +5,9 @@ import { Database } from './database';
 export type Ingredient = Database['public']['Tables']['ingredient']['Row'];
 export type Recipe = Database['public']['Tables']['recipe']['Row'];
 export type RecipeIngredientJoined = Database['public']['Views']['recipe_ingredient_joined']['Row'];
+export type ShoppingList = Database['public']['Tables']['shopping_list']['Row'];
+export type ShoppingListJoined = Database['public']['Views']['shopping_list_joined']['Row'];
+export type ShoppingListRecipeJoined = Database['public']['Views']['shopping_list_recipe_joined']['Row'];
 
 // DTO (Data Transfer Object) types
 export interface IngredientDto {
