1a0ea44cc9d4734a0cae595527621c5cfe49ced8
beau-haldane <beaujorgensen@gmail.com>
2024-01-27 14:16:34 +1000

    remove unused function

--- utils/recipes.ts
+++ utils/recipes.ts
@@ -68,23 +68,3 @@ export async function getRecipeIngredients(recipe: Recipe): Promise<RecipeDto> {
     ...recipeCostPerServe,
   };
 }
-
-export async function getRecipeIngredientsOld(
-  recipeIds: Array<number>
-): Promise<DbRecipeIngredientDetailed[]> {
-  return await db('recipe_ingredient as ri')
-    .select(
-      'i.id as ingredient_id',
-      'i.name',
-      'i.category',
-      'i.cost_per',
-      'i.number_of',
-      'i.measurement_unit',
-      'i.category',
-      'ri.recipe_id',
-      'ri.number_of as recipe_number_of',
-      'ri.measurement_unit as recipe_measurement_unit'
-    )
-    .leftJoin('ingredient as i', 'i.id', 'ri.ingredient_id')
-    .whereIn('ri.recipe_id', recipeIds);
-}
